fb913f3c975d7fbef07ecfcf2b65be5481e2704b
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-22 23:58:13 +0100

    activating hot reload

--- Flask alchemy/flaskHelloApp.py
+++ Flask alchemy/flaskHelloApp.py
@@ -2,14 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://posgtres:1475963mimo@localhost:5432/udacity'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/udacity'
 
 db = SQLAlchemy(app)
 class Person(db.Model):
     __tablename__ = "personnes"
     id = db.Column(db.Integer, primary_key= True)
-    name = db.Column(db.String(), nullable= False)
+    name = db.Column(db.String(), nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    person = Person.query.first()
+    return 'Hello! '+ person.name
