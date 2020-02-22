27bc660fade4d85b619a6029ab53a3a2afcc51b3
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-22 19:25:55 +0100

    creating model

--- Flask alchemy/flaskHelloApp.py
+++ Flask alchemy/flaskHelloApp.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://posgtres:1475963mimo@localhost:5432/udacity'
+
+db = SQLAlchemy(app)
+class Person(db.Model):
+    __tablename__ = "personnes"
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(), nullable= False)
 
 @app.route('/')
 def index():
