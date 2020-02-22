e4e10e7ac580487a11fb6b5a942e5f0682b11de7
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-23 00:11:10 +0100

    disable track modifications

--- Flask alchemy/flaskHelloApp.py
+++ Flask alchemy/flaskHelloApp.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/udacity'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 db = SQLAlchemy(app)
 class Person(db.Model):
