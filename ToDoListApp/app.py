dd3f6a63217bf506c88a93f6d7fe6786ab11209d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-23 15:17:59 +0100

    creating todo class

--- ToDoListApp/app.py
+++ ToDoListApp/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class ToDoList(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    description = db.Column(db.String(), nullable= False)
+    def __repr__(self):
+        return f'< ToDo ID: {self.id} , description : {self.description} >'
 
 @app.route('/')
 def index():
