714310f2659f4f036bea632ea23fb6a6adbd037e
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-23 15:09:39 +0100

    update project

--- ToDoListApp/app.py
+++ ToDoListApp/app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',data=[{
+        'description': 'todo1'
+    },
+    {
+        'description': 'todo2'
+    },
+    {
+        'description': 'todo3'
+    },
+    ])
     
