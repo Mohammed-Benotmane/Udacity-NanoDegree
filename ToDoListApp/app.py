8a548bbd057c4ca2353576fcf57a259e8aa23a3c
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-24 12:48:23 +0100

    first migration

--- ToDoListApp/app.py
+++ ToDoListApp/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres@localhost:5432/udacity'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
-
+migrate = Migrate()
 class ToDoList(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key= True)
