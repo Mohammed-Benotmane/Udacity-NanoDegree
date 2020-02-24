be801c5c5dddb8b7813bbeb50453599c4cacbc5d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-24 13:38:06 +0100

    update project

--- ToDoListApp/app.py
+++ ToDoListApp/app.py
@@ -12,10 +12,9 @@ class ToDoList(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key= True)
     description = db.Column(db.String(), nullable= False)
+    completed = db.Column(db.Boolean, nullable= False,default=False)
     def __repr__(self):
         return f'< ToDo ID: {self.id} , description : {self.description} >'
-
-db.create_all()
 
 @app.route('/')
 def index():
