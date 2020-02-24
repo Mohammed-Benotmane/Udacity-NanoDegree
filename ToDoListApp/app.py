23ed53fe0b3ea0284c2a4a0a8ae29a25bd91a63c
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-02-24 11:31:50 +0100

    add form

--- ToDoListApp/app.py
+++ ToDoListApp/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres@localhost:5432/udacity'
-app.cofig['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
 class ToDoList(db.Model):
@@ -18,4 +18,14 @@ db.create_all()
 @app.route('/')
 def index():
     return render_template('index.html',data=ToDoList.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create():
+    descr = request.get_json()['description']
+    db.session.add(ToDoList(description=descr))
+    db.session.commit()
+    print(descr)
+    return jsonify({
+        'description' : descr
+    })
     
