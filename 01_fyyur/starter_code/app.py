9e6eabeae8060feaaa8a641434ddc561d5023d59
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-03 01:31:25 +0100

    add show detail

--- 01_fyyur/starter_code/app.py
+++ 01_fyyur/starter_code/app.py
@@ -100,39 +100,13 @@ def venues():
   #       num_shows should be aggregated based on number of upcoming shows per venue.
   
 
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0
-    }]
-  }]
-  
-  
+  data=[]
   selected = db.session.query(Venue).distinct('city',).all()
-  
-  print(shows)
-  
   for select in selected:
     venues = Venue.query.filter(Venue.city == select.city)
     temp= []
     for venu in venues:
       upcomingCount = Show.query.filter(Show.venue_id == venu.id, Show.start_time < datetime.now()).count()
-      print(upcomingCount)
       temp.append(
       {
       "id": venu.id,
@@ -177,27 +151,34 @@ def show_venue(venue_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
   venueTemp = Venue.query.get(venue_id)
+  pastShowsquery= Show.query.filter(Show.venue_id == venueTemp.id, Show.start_time < datetime.now()).all()
+  pastShowsCount = len(pastShowsquery)
+  past_shows = []
+
+  for past in pastShowsquery:
+    past_shows.append({
+      "artist_id": past.artist_id,
+      "artist_name": Artist.query.get(past.artist_id).name,
+      "artist_image_link": Artist.query.get(past.artist_id).image_link,
+      "start_time": str(past.start_time)
+    })
+
   data1={
-    "id": 1,
-    "name": "The Musical Hop",
+    "id": venueTemp.id,
+    "name": venueTemp.name,
     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    "past_shows": [{
-      "artist_id": 4,
-      "artist_name": "Guns N Petals",
-      "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-      "start_time": "2019-05-21T21:30:00.000Z"
-    }],
+    "address": venueTemp.address,
+    "city": venueTemp.city,
+    "state": venueTemp.state,
+    "phone": venueTemp.phone,
+    "website": venueTemp.website,
+    "facebook_link": venueTemp.facebook_link,
+    "seeking_talent": venueTemp.seeking_talent,
+    "seeking_description": venueTemp.seeking_description,
+    "image_link": venueTemp.image_link,
+    "past_shows":past_shows,
     "upcoming_shows": [],
-    "past_shows_count": 1,
+    "past_shows_count": pastShowsCount,
     "upcoming_shows_count": 0,
   }
   data2={
