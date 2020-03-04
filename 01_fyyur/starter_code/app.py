594286ce71b6bb517130bcbe02ffb0e38b37e7ad
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-04 14:59:33 +0100

    adding create artist

--- 01_fyyur/starter_code/app.py
+++ 01_fyyur/starter_code/app.py
@@ -423,14 +423,21 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  error = False
+  try:
+    artistTemp = Artist(name=request.form['name'],city=request.form['city'],state=request.form['state'],phone= request.form['phone'],genres=request.form['genres'],image_link='',facebook_link=request.form['facebook_link'],website='', seeking_venue=False,seeking_description='')
+    db.session.add(artistTemp)
+    db.session.commit()
+  except:
+    error = True
+    db.session.rollback()  
+  finally:
+    db.session.close()
+    if error:
+      flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+    else:
+      flash('Artist ' + request.form['name'] + ' was successfully listed!')
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
   return render_template('pages/home.html')
 
 
