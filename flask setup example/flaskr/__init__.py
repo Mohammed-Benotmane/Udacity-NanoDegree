9bc671f303b987a321de392443beaf2ccb7212c0
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-09 14:13:41 +0100

    add models

--- flask setup example/flaskr/__init__.py
+++ flask setup example/flaskr/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask,jsonify
+from models import setup_db, Plant
+from flask_cors import CORS
 
 def create_app(test_config =None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
     @app.route('/')
     def hello():
         return jsonify({
