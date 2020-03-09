b9620648d96b439887dc17f8b80e15c1ed289ed6
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-09 14:22:02 +0100

    update init

--- flask setup example/flaskr/__init__.py
+++ flask setup example/flaskr/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask,jsonify
-from models import setup_db, Plant
+from models import setup_db, Book
 from flask_cors import CORS
 
 def create_app(test_config =None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods','GET,POST,DELETE,OPTIONS,PUT')
+        return response
 
     @app.route('/')
     def hello():
