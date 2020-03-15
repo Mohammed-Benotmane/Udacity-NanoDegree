437ce41bb4e7058a9a1c53ea15d9194bfaa606da
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-15 14:55:00 +0100

    add unitttest

---
+++ flask setup example/flaskr/test_flaskr.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+import json
+
+from flaskr import create_app
+from models import setup_db, Book
+
+class BookTestCase(unittest.TestCase):
+    def setup(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "fyyur"
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres','postgres','localhost:5432',self.database_name)
+        setup_db(self.app, self.database_path)
+
+        self.new_book = {
+            'title':'Anansi Boys',
+            'author':'Zakaria',
+            'rating': 5
+        }
+    
+    def tearDown(self):
+        pass
+
+    def test_get_pagination_books(self):
+        res = self.client().get('/books')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['total_books'])
+        
+
+
