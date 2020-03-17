ff604ecb12471c15d39978b51f65abe600baaf77
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-17 15:57:23 +0100

    add delete question

--- 02_trivia_api/starter/backend/flaskr/__init__.py
+++ 02_trivia_api/starter/backend/flaskr/__init__.py
@@ -73,6 +73,19 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>',methods=['DELETE'])
+  def delete_question(question_id):
+    question = Question.query.filter(Question.id == question_id).one_or_none()
+    if question is None:
+      abort(404)
+    question.delete()
+    questions = Question.query.all()
+    return jsonify({
+      'success':True,
+      'deleted':question_id,
+      'questions':[question.format() for question in questions]
+    })
+
 
   '''
   @TODO: 
