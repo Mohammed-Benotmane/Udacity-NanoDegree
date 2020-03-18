8dc0daa9c58a0c13452d478db7901e5eae3b36a2
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-18 16:47:49 +0100

    add error handler 404

--- 02_trivia_api/starter/backend/flaskr/__init__.py
+++ 02_trivia_api/starter/backend/flaskr/__init__.py
@@ -210,6 +210,14 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
   @app.route('/')
   def hello():
     return jsonify({
