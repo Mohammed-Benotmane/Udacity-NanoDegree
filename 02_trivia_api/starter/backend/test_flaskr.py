bd3cb6696f3e563c8172b741d1fc237969dbd8cf
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-03-18 21:45:13 +0100

    update test_flaskr

--- 02_trivia_api/starter/backend/test_flaskr.py
+++ 02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', '','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
