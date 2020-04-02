456e60bb717d94abb60ea78b3fe648a1b204896d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-02 18:40:58 +0200

    add get token function

--- Sending Tokens/app.py
+++ Sending Tokens/app.py
@@ -2,11 +2,22 @@ from flask import Flask,request,abort
 
 app = Flask(__name__)
 
-@app.route('/headers')
-def headers():
+def get_token_auth_header():
     if 'Authorization' not in request.headers:
         abort(401)
     test = request.headers['Authorization']
-    token = test.split(' ')[1]
-    print(token)
+    token = test.split(' ')
+
+    if len(token) != 2:
+        abort(401)
+    elif token[0].lower() != 'bearer':
+        abort(401)
+
+    
+    return token[1]
+
+@app.route('/headers')
+def headers():
+    jwt = get_token_auth_header()
+    print(jwt)
     return 'nothing'
