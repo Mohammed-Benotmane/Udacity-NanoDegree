96abe47657e9edae3041fcceff94c8217fca9400
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-02 18:30:30 +0200

    handle 401 error

--- Sending Tokens/app.py
+++ Sending Tokens/app.py
@@ -1,9 +1,11 @@
-from flask import Flask,request
+from flask import Flask,request,abort
 
 app = Flask(__name__)
 
 @app.route('/headers')
 def headers():
+    if 'Authorization' not in request.headers:
+        abort(401)
     test = request.headers['Authorization']
     token = test.split(' ')[1]
     print(token)
