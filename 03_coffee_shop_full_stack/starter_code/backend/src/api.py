a80ec4ea5bec6a962e5cdcb37101079239666822
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-12 23:55:58 +0200

    add get drinks first try

--- 03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ 03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -27,9 +27,10 @@ db_drop_and_create_all()
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks')
+@app.route('/drinks',methods=['GET'])
 def get_drinks():
-    return 'test'
+    drinks = Drink.query.all()
+    return jsonify(drinks)
 
 '''
 @TODO implement endpoint
