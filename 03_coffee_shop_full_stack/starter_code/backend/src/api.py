823908f411327cee5c3dda7addd33a209ae09c92
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-12 23:48:35 +0200

    change pip

--- 03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ 03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -27,7 +27,9 @@ db_drop_and_create_all()
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks')
+def get_drinks():
+    return 'test'
 
 '''
 @TODO implement endpoint
