db7c45f46d0e03be5c4dffd26daca308c4c4ee69
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-15 01:59:47 +0200

    add 404 handler

--- 03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ 03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -148,6 +148,13 @@ def unprocessable(error):
                     }), 404
 
 '''
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
 
 '''
 @TODO implement error handler for 404
