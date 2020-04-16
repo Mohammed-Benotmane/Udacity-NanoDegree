7787dcb8a86fd3e3a5ad601c72c107f5b8b28c37
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-04-16 04:11:43 +0200

    finalize project

--- 03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ 03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -2,92 +2,59 @@ import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
+import sys
 from flask_cors import CORS
-from .database.models import db_drop_and_create_all, setup_db, Drink
+from .database.models import db_drop_and_create_all, setup_db, Drink,db
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
 #db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks',methods=['GET'])
 def get_drinks():
     drinks = Drink.query.all()
     formatted_drink = [drk.short() for drk in drinks]
 
     return jsonify({
-        'Success':True,
-        'Drinks': formatted_drink
+        'success':True,
+        'drinks': formatted_drink
     })
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
-@app.route('/drinks-details',methods=['GET'])
-def get_drinks_details():
+
+@app.route('/drinks-detail',methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_details(token):
     drinks = Drink.query.all()
     formatted_drink = [drk.long() for drk in drinks]
-
+    print(formatted_drink)
     return jsonify({
-        'Success':True,
-        'Drinks': formatted_drink
+        'success':True,
+        'drinks': formatted_drink
     })
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks',methods=['POST'])
 @requires_auth("post:drinks")
 def post_drink(token):
     body = request.get_json()
     new_title = body.get("title",None)
     new_recipe = body.get("recipe",None)
-    new_recipe =  json.dumps(new_recipe)
-    str_recipe = str(new_recipe)
-    drink = Drink(title = new_title,recipe= str_recipe)
-    drink.insert()
+    toz =  json.dumps(new_recipe)
+    str_recipe = str(toz)
+    try:
+        drink = Drink(title = new_title,recipe= str_recipe)
+        drink.insert()
+    except:
+        print(sys.exc_info())
+        db.session.rollback()
+        return unprocessable(422)
     return jsonify({
         'Success':True,
         'Drinks': drink.long()
     })
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks/<drink_id>", methods=['PATCH'])
 @requires_auth("patch:drinks")
 def patch_drinks(token ,drink_id):
@@ -106,16 +73,6 @@ def patch_drinks(token ,drink_id):
         "drinks": drink.long()
     })
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks/<drink_id>", methods=['DELETE'])
 @requires_auth("delete:drinks")
 def delete_drinks(token, drink_id):
@@ -140,16 +97,6 @@ def unprocessable(error):
                     "message": "unprocessable"
                     }), 422
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({
@@ -158,16 +105,6 @@ def resource_not_found(error):
                     "message": "resource not found"
                     }), 404
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
 @app.errorhandler(AuthError)
 def handle_auth_error(err):
     response = jsonify(err.error)
--- 03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ 03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,7 +1,3 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
